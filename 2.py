403321e37e46df97b2ffdc4b602df1116bcc55bf
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2024-09-28 14:22:40 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,14 @@
+a = 25
+b = 96.354
+c = True
+d = "Hello"
+e = [2, 5, 9, 4]
+f = {"A", "B", "C", "D"}
+g = ("268","86","375","673")
+print(f"The value of variable a is {a}, its datatype is {type(a)}")
+print(f"The value of variable b is {b}, its datatype is {type(b)}")
+print(f"The value of variable c is {c}, its datatype is {type(c)}")
+print(f"The value of variable d is {d}, its datatype is {type(d)}")
+print(f"The value of variable e is {e}, its datatype is {type(e)}")
+print(f"The value of variable f is {f}, its datatype is {type(f)}")
+print(f"The value of variable g is {g}, its datatype is {type(g)}")
